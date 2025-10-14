592a67b505804774fa0229dd28e07543197e6f8f
nishantgupta08 <nishantthewriter@gmail.com>
2025-10-14 21:25:22 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,12 +1,52 @@
-import CounsellingForm from "@/components/counelling_form";
+Turbopack
+Build Error
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="min-h-screen antialiased">
-        {children}
-        <CounsellingForm /> {/* mounted globally so any page can open it */}
-      </body>
-    </html>
-  );
-}
+
+Ecmascript file had an error
+
+./app/page.tsx (25:1)
+
+Ecmascript file had an error
+  23 | }
+  24 |
+> 25 | export default function Home() {
+     | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+> 26 |   return (
+     | ^^^^^^^^^^
+> 27 |     <>
+     | ^^^^^^^^^^
+> 28 |       <SocialBadge />
+     | ^^^^^^^^^^
+> 29 |       <HeroSection />
+     | ^^^^^^^^^^
+> 30 |       {/* <Features /> */}
+     | ^^^^^^^^^^
+> 31 |       <CounsellingForm />
+     | ^^^^^^^^^^
+> 32 |       <FellowshipPrograms />
+     | ^^^^^^^^^^
+> 33 |       {/* <Courses /> */}
+     | ^^^^^^^^^^
+> 34 |       <CourseSectionPro />
+     | ^^^^^^^^^^
+> 35 |       <Mentors />
+     | ^^^^^^^^^^
+> 36 |       <Testimonials />
+     | ^^^^^^^^^^
+> 37 |       <WhoCanApply />
+     | ^^^^^^^^^^
+> 38 |       {/* <Gallery /> */}
+     | ^^^^^^^^^^
+> 39 |       <WorkshopGallery />
+     | ^^^^^^^^^^
+> 40 |
+     | ^^^^^^^^^^
+> 41 |     </>
+     | ^^^^^^^^^^
+> 42 |   );
+     | ^^^^^^^^^^
+> 43 | }
+     | ^^
+  44 |
+
+the name `default` is exported multiple times
