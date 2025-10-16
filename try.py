fec50945c8750d1c6a86cebb144b7cd8903321b8
nishantgupta08 <nishantthewriter@gmail.com>
2025-10-16 20:36:35 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,13 +1,13 @@
-Jagannath University jaipur 	BTech
-Post graduate programs in Data science and analytics 	BTech
-NIT Rourkela	MSc
-Poornima college of engineering 	BTech
-Dr BR Ambedkar Open University	BA
-Electronics and computer engineering 	B.E 
-IIIT Bangalore 	Pg diploma 
-Rajasthan University, CS & IT Department 	M.sc IT
-B.Tech ( Electronics and Control )	BTech
-RCEW	MTech
-SBCET	BTech
-Sri Balaji College of Engineering and Technology 	BTech
-Maharishi Arvind School of Management Studies	BCA
+341533	Jagannath University jaipur 	BTech
+322023	Post graduate programs in Data science and analytics 	BTech
+751030	NIT Rourkela	MSc
+302031	Poornima college of engineering 	BTech
+500035	Dr BR Ambedkar Open University	BA
+311001	Electronics and computer engineering 	B.E 
+700118	IIIT Bangalore 	Pg diploma 
+302034	Rajasthan University, CS & IT Department 	M.sc IT
+302021	B.Tech ( Electronics and Control )	BTech
+302034	RCEW	MTech
+302012	SBCET	BTech
+302012	Sri Balaji College of Engineering and Technology 	BTech
+302012	Maharishi Arvind School of Management Studies	BCA
